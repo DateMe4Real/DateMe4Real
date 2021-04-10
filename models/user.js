911d870bef9aff37f80d91bf6711ee27fd5dbe2c
Qi Feng <qi.feng86@gmail.com>
2021-04-10 11:11:20 -0400

    fix indexjs in models folder and modified seedDb.js

--- models/user.js
+++ models/user.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema(
-  {
-    username: {
-      type: String,
-      trim: true,
-      unique: true,
-    },
-    uuid: {
-      type: String,
-      trim: true,
-      unique: true,
-    },
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
   },
+  uuid: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+},
   { strict: false }
 );
+
 
 const User = mongoose.model("User", UserSchema);
 
