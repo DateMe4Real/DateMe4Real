504c2fb29bccebdb65d97590b20171302f052be8
Jeff Woda <wodahouse@gmail.com>
2021-04-10 12:15:29 -0400

    adding default values for match collection (#11)

--- models/match.js
+++ models/match.js
@@ -6,16 +6,19 @@ const MatchSchema = new Schema({
   matches: {
     type: Boolean,
     required: true,
+    default: false,
   },
 
   userchoice: {
     type: Boolean,
     required: true,
+    default: false,
   },
 
   matched: {
     type: Boolean,
     required: true,
+    default: false,
   },
 });
 
