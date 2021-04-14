7eb0afd3cb8964d416173944f9700c5070a1d174
Jeff Woda <wodahouse@gmail.com>
2021-04-14 19:12:15 -0400

    fixed proxy server issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Welcome from "./pages/welcome";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 const App = () => {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
+// import awsconfig from "./aws-exports";
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
