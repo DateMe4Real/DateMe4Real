5c76440e1bfd4e881b6d6e56de3ed3b7a0391336
Jeff Woda <wodahouse@gmail.com>
2021-04-14 18:09:35 -0400

    fixing random models import feature on front end to back end (doesn't work) (#26)
    * adding default values for match collection
    
    * adding workflow charts
    
    * updating welcome page
    
    * updated checkUser API logic
    
    * fixing App.js pull conflict
    
    * fixing AWS refernce files in two sections App and welcome
    
    * fixing random reference to models don't belong on front end

--- client/src/pages/profileInput.js
+++ client/src/pages/profileInput.js
@@ -1,65 +1,78 @@
-const Profile = require("../../../models/profile");
-
 class ProfileForm extends Profile {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    handleSubmit(event) {
-        console.log(this.state.value);
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Sex:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Sex:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
 
-                <label>
-                    Date of Birth:
-                    <input type="date" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+        <label>
+          Date of Birth:
+          <input
+            type="date"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
 
-                <label>
-                    Race:
-                    <select value={this.state.value} onChange={this.handleChange}>
-                        <option value="black">Black/African American</option>
-                        <option value="spanish">Hispanic/Latino</option>
-                        <option value="asian">Asian</option>
-                        <option value="indian">Indian/Native American</option>
-                        <option value="white">White/Caucasian</option>
-                    </select>
-                </label>
-                <input type="submit" value="Submit" />
+        <label>
+          Race:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="black">Black/African American</option>
+            <option value="spanish">Hispanic/Latino</option>
+            <option value="asian">Asian</option>
+            <option value="indian">Indian/Native American</option>
+            <option value="white">White/Caucasian</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
 
-                <label>
-                    Bodystyle:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+        <label>
+          Bodystyle:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
 
-                <label>
-                    Bio:
-                     <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-
-            </form>
-        );
-    }
+        <label>
+          Bio:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 module.exports = ProfileForm;
