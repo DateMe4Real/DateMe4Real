a152e522231d85a2e5221bc53d8880e7f2d2052e
Jeff Woda <wodahouse@gmail.com>
2021-04-14 17:49:20 -0400

    Update API CheckUser (#23)
    * adding default values for match collection
    
    * adding workflow charts
    
    * updating welcome page
    
    * updated checkUser API logic
    
    * fixing App.js pull conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Welcome from "./pages/welcome";
-<<<<<<< HEAD
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { Auth } from "aws-amplify";
-=======
->>>>>>> bf4dea79176fc8dd2337505da11b82236ef245a6
+
 
 const App = () => {
   return (
