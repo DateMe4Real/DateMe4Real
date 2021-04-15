00c7e2b807c561f52a70ea519c47a418c4c54d46
Jeff Woda <wodahouse@gmail.com>
2021-04-15 19:02:48 -0400

    fixing api routes

--- client/src/pages/welcome.js
+++ client/src/pages/welcome.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Auth } from "aws-amplify";
 import API from "../utils/API";
 
+var _id;
+// save as state in global variable
+
 function Welcome() {
+  // useEffect(() => {
+  //   checkUser();
+  // }, []);
+
   const checkUser = async () => {
     const user = await Auth.currentAuthenticatedUser();
-    const uuid = user.attributes.sub;
+    _id = user.attributes.sub;
     const username = user.username;
-    const ID = await API.checkUser(uuid, username);
-    console.log({ ID });
+    const results = await API.checkUser2(_id, username);
+    console.log(results);
   };
 
   return <div>{<button onClick={checkUser}> Check Function </button>}</div>;
--- routes/api/welcome.js
+++ routes/api/welcome.js
@@ -3,4 +3,6 @@ const usersController = require("../../controllers/usersController");
 
 router.route("/userCheck/:uuid/:username").get(usersController.userCheck);
 
+router.route("/userCheck2/:_id/:username").get(usersController.userCheck2);
+
 module.exports = router;
